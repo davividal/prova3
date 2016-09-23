6375e6ba1372cf0c5d3bc49e585bae0b52dc72fe
Davi Koscianski Vidal <davividal@gmail.com>
2016-09-23 20:42:15 -0300

    Layout; normal button

--- app/src/main/java/com/example/exam/MainActivity.java
+++ app/src/main/java/com/example/exam/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.exam;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private static Integer normalCalls = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        normalCalls = 0;
+    }
+
+    public void sleep5(View view) {
+    }
+
+    public void count5(View view) {
+    }
+
+    public void normal(View view) {
+        String calls = String.format(Locale.getDefault(), " %d chamadas", ++normalCalls);
+        ((TextView)findViewById(R.id.normal)).setText(calls);
     }
 }
